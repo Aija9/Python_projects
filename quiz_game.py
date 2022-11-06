d36feb1b2443176333d8238180a1837849da43b0
Aija Akesh <aija.akesh@student.kit.edu>
2022-11-06 15:43:02 +0100

    finished quiz game

--- quiz_game.py
+++ quiz_game.py
@@ -7,17 +7,19 @@ if playing.lower() != "yes":
     quit()
 
 print("OKay! Let's play :)")
+score = 0
 
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print('Correct!')
-
+    score += 1
 else: 
     print("Incorrect!")
 
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
     print('Correct!')
+    score += 1
 
 else: 
     print("Incorrect!")
@@ -26,6 +28,7 @@ else:
 answer = input("What does RAM stand for? ")
 if answer.lower() == "Random access memory":
     print('Correct!')
+    score += 1
 
 else: 
     print("Incorrect!")
@@ -33,6 +36,10 @@ else:
 answer = input("What does PSU stand for? ")
 if answer.lower() == "Power supply":
     print('Correct!')
+    score += 1
 
 else: 
     print("Incorrect!")
+
+
+print("You got " + str(score) + "questions correct!")
